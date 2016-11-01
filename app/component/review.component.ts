ca119c2b8fd60a6bd734845993a284eb447cfd52
blindacai <linda_cai2014@hotmail.com>
2016-11-01 11:03:37 -0700

    add back file conditions

--- app/component/review.component.ts
+++ app/component/review.component.ts
@@ -28,7 +28,9 @@ import {Library} from "../service/library";
 export class ReviewComponent implements OnInit{
   id: number;
   lib: Library = null;
-  filecontent: any;
+
+  filecontent: string;
+
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -38,8 +40,6 @@ export class ReviewComponent implements OnInit{
     this.route.params.subscribe(params => this.id = Number.parseInt(params["id"]));
     this.libraryservice.getLibById(this.id).subscribe(data => this.lib = data);
     //console.log(this.lib); // is null; asyn
-
-    
   }
 
   // $event.target === <input type="file"
@@ -48,11 +48,20 @@ export class ReviewComponent implements OnInit{
   }
 
   readFile(inputValue: any) : void {
-    var file:File = inputValue.files[0]; 
-    var myReader:FileReader = new FileReader();
+    var file = inputValue.files[0];
+    var fileType = /text.*/;
 
-    myReader.onloadend = (ev) => { this.filecontent = myReader.result };
-    myReader.readAsText(file);
+    if(file == null){
+      this.filecontent = null;
+      return;
+    }
+
+    if(file.type.match(fileType)){
+      var myReader: FileReader = new FileReader();
+      myReader.onloadend = (ev) => { this.filecontent = myReader.result };
+      myReader.readAsText(file);
+    }
+    else this.filecontent = "File not supported!";
   }
 
 
