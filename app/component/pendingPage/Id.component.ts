e15928a75efe57c69fb0851fb6b1e82c683b2b70
blindacai <linda_cai2014@hotmail.com>
2016-11-28 15:27:36 -0800

    highlight id when lib status is changed

--- app/component/pendingPage/Id.component.ts
+++ app/component/pendingPage/Id.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import {Router} from "@angular/router";
 import {Library} from "../../service/model/library";
 
@@ -7,36 +7,24 @@ import {Library} from "../../service/model/library";
     selector: '[id-field]',
     template: `
                 <td align = "center"><span (click) = "onSelect(library.id)">
-                                            <p [theHighlight] = "color">{{library.id}}</p>
+                                            <p [theHighlight] = "newstatus">{{library.id}}</p>
                                      </span>
                 </td>
               `
 })
 
-export class IdField implements OnInit{
+export class IdField{
 
     @Input('id-field')
     library: Library;
 
-    color: string;
+    @Input()
+    newstatus: string;
 
     constructor( private router: Router) { }
-
-    ngOnInit(){
-        this.changeColor();
-    }
 
   onSelect(idfield: number){
     this.router.navigate(['/review', idfield]);
   }
 
-  changeColor(){
-      let status = this.library.status;
-      if(status == 'Pending')
-          this.color = "white";
-      else if(status == 'Passed')
-          this.color = "green";
-      else if(status == 'Failed')
-          this.color = "red";
-  }
 }
--- app/directive/highlight.directive.ts
+++ app/directive/highlight.directive.ts
@@ -1,20 +1,20 @@
-import { Directive, ElementRef, Input, Renderer, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer, OnChanges } from '@angular/core';
 
 @Directive({
     selector: '[theHighlight]'
 })
 
-export class HighlightDirective implements OnInit{
+export class HighlightDirective implements OnChanges{
     @Input('theHighlight')
-    color: string = "red";
+    color: string;
 
     constructor(private el: ElementRef, private renderer: Renderer) {}
 
-    ngOnInit(){
+    ngOnChanges(){
         this.highlight(this.color);
     }
 
     private highlight(color: string) {
-        this.renderer.setElementStyle(this.el.nativeElement, 'background-color', color);
+        this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', color);
     }
 }
